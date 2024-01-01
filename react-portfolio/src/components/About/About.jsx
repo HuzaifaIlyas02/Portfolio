594869f4d72b8531d95ed14fc1a56255dbc1e1e7
HuzaifaIlyas02 <huzaifailyas02@outlook.com>
2024-01-01 20:37:46 +0500

    Updated Hero Section

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
     <section className={styles.container} id="about">
         <h2 className={styles.title}>About</h2>
         <div className={styles.content}>
-            <img src={getImageUrl("about/aboutImage.png")} alt="Me sitting with a laptop" className={styles.aboutImage}/>
+            <img src={getImageUrl("about/about.png")} alt="Me sitting with a laptop" className={styles.aboutImage}/>
             <ul className={styles.aboutItems}>
                 <li className={styles.aboutItem}>
                 <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon"/>
